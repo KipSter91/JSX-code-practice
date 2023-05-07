46708381aaf322d34679f7f0ae5eec917322e3ef
Zsolt Marku <123772528+KipSter91@users.noreply.github.com>
2023-05-07 11:24:22 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World</h1>
+    <ul>
+      <li>Item 01</li>
+      <li>Item 02</li>
+      <li>Item 03</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
